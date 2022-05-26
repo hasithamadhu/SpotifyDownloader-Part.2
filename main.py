be44d9067461eb0441c86e693f21bb251bc76e54
hasithamadhu <98018241+hasithamadhu@users.noreply.github.com>
2022-05-26 11:28:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import wave, math, os, json, shutil, subprocess, asyncio, time
 from pyrogram import Client, filters
-from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from vosk import Model, KaldiRecognizer
-from display_progress import progress_for_pyrogram, download_progress_hook, read_stderr
-from yt_dlp import YoutubeDL
 from tqdm import tqdm
 from fpdf import FPDF
 import requests
@@ -13,15 +9,9 @@ import requests
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
-# vosk supported language(code), see supported languages here: https://github.com/alphacep/vosk-api
-LANGUAGE_CODE = os.environ.get("LANGUAGE_CODE", "en-us")
-# language model download link (see available models here: https://alphacephei.com/vosk/models)
-MODEL_URL = os.environ.get("MODEL_DOWNLOAD_URL", "https://alphacephei.com/vosk/models/vosk-model-en-us-0.22-lgraph.zip")
-# transcript sending format (PDF or TXT)
-TRANSCRIPT_FORMAT = os.environ.get("SEND_AS", "TXT")
 
 Bot = Client(
-    "Transcript-Extractor-Bot",
+    "SpotifyDownloader-Part.2",
     bot_token = BOT_TOKEN,
     api_id = API_ID,
     api_hash = API_HASH
